af39f0e640ea96da8172ab757a800dec528518e0
Michail Tsagris <mtsagris@uoc.gr>
2023-03-22 12:40:23 +0000

    version 0.9

--- R/big_read.R
+++ R/big_read.R
@@ -1,4 +1,4 @@
-big_read <- function(big_path, header = TRUE, sep = ",") {
-  x <- bigstatsr::big_read(big_path, header = header, sep = sep, type = "double")
+big_read <- function(big_path, select, header = TRUE, sep = ",") {
+  x <- bigstatsr::big_read(big_path, header = header, sep = sep, type = "double", select = select )
   bigstatsr::big_copy(x)
 }
--- R/bnplot.R
+++ R/bnplot.R
@@ -1,3 +1,3 @@
-bnplot <- function(dag, shape = "ellipse", main = NULL, sub = NULL) {
-  bnlearn::graphviz.plot(x = dag, shape = shape, main = main, sub = sub)
+bnplot <- function(dag, shape = "ellipse", main = NULL, sub = NULL, highlight = NULL) {
+  bnlearn::graphviz.plot(x = dag, shape = shape, main = main, sub = sub, highlight = highlight)
 }
